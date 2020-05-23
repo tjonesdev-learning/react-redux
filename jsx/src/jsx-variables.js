05e525dc193c0331e9e4b80ffab11d42bd65dde0
Tracey <tracey.l.jones@outlook.com>
2020-05-23 16:30:53 -0500

    Created mini project for variable rules in jsx

---
+++ jsx/src/jsx-variables.js
@@ -0,0 +1,23 @@
+// Import the React and ReactDOM libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create React component
+const App = () => {
+const buttonText = 'Click Me!';
+
+  return (
+    <div>
+      <label class="label" for="name">
+        Enter name:
+      </label>
+      <input id="name" type="text" />
+      <button style={{backgroundColor: 'blue', color: 'white'}}>
+        {buttonText}
+      </button>
+    </div>
+  );
+}
+
+// Display React component to DOM
+ReactDOM.render(<App />, document.querySelectory('#root'));

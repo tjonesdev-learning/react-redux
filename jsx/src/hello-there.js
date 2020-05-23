2cad30fd156a46168d9bd7a08a91d7d50929669d
Tracey <tracey.l.jones@outlook.com>
2020-05-23 15:44:23 -0500

    Created mini-project "hello-there"

---
+++ jsx/src/hello-there.js
@@ -0,0 +1,11 @@
+// Import the React and ReactDOM libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create React componen
+const App = () => {
+  return <div>Hello there!</div>;
+}
+
+// Display React component to DOM
+ReactDOM.render(<App />, document.querySelectory('#root'));

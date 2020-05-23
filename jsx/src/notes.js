813f14f0d553c47eebf9ab6ff18c9ea99a087d95
Tracey <tracey.l.jones@outlook.com>
2020-05-23 16:32:57 -0500

    Created notes.js as separate file

---
+++ jsx/src/notes.js
@@ -0,0 +1,6 @@
+// **NOTE on Common conventions:**
+
+// Double quotes ("") for JSX properties (i.e. <label for="name"></label>)
+// Single quotes ('') for non-JSX (i.e. style={{color='white'}})
+
+// Use "className" instead of "class" in JSX to avoid collisions over Javascript "class"

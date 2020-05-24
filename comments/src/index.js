4ccb3a83a1fe1d9886d8bed0fcfdd714406aa60f
Tracey <tracey.l.jones@outlook.com>
2020-05-24 11:59:10 -0500

    added warning card

--- comments/src/index.js
+++ comments/src/index.js
@@ -7,6 +7,12 @@ import ApprovalCard from "./ApprovalCard";
 const App = () => {
 	return (
 		<div className="ui container comments">
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
 			<ApprovalCard>
 				<CommentDetail
 					author="Sam"

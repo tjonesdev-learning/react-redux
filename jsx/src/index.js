edcc2d9f9209e21fb9f55cd52d45cf47112e41b8
Tracey <tracey.l.jones@outlook.com>
2020-05-24 10:47:26 -0500

    Imported components

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,8 +1,23 @@
 // Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
+import HelloThere from './HelloThere';
+import Styling from './Styling';
+import Variables from './Variables';
+import Functions from './Functions';
+import Objects from './Objects';
 
-
+const App = () => {
+  return (
+    <div>
+      <HelloThere />
+      <Styling />
+      <Variables />
+      <Functions />
+      <Objects />
+    </div>
+  );
+}
 
 // Display React component to DOM
-ReactDOM.render(<App />, document.querySelectory('#root'));
+ReactDOM.render(<App />, document.querySelector('#root'));

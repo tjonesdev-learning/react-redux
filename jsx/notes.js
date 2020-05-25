a995365d6e9a30286c43b96cccb67d6b99ac92b6
Tracey <tracey.l.jones@outlook.com>
2020-05-24 20:23:01 -0500

    Moved notes to root folder of react-redux and changed to text file; added note on rendering functions

--- jsx/notes.js
+++
@@ -1,8 +0,0 @@
-// **NOTE on Common conventions:**
-
-// Double quotes ("") for JSX properties (i.e. <label for="name"></label>)
-// Single quotes ('') for non-JSX (i.e. style={{color='white'}})
-
-// Use "className" instead of "class" in JSX to avoid collisions over Javascript "class"
-
-// Use "htmlFor" instead of "for" in JSX to differentiate from Javascript for loops

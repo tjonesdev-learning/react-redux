d8e92d8d8eca1c3b222d0feaa0c08419d6182577
Tracey <tracey.l.jones@outlook.com>
2020-05-24 20:06:14 -0500

    Re-factored to class based component from functional based

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,15 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
-const App = () => {
+/* 
+  const App = () => {
   window.navigator.geolocation.getCurrentPosition(
     (position) => console.log(position),
     (err) => console.log(err)
   );
 
   return (
-    <div>Hi there!</div>
+    <div>Latitude: </div>
   );
-};
+}; 
+*/
+
+// Re-factored to class based component
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    );
+
+    return <div>Latitude: </div>
+  };
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));

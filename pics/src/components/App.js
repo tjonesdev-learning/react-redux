bfa2f75da8fdc720c9190fe49a673f83d33d6af8
Tracey <tracey.l.jones@outlook.com>
2020-05-25 16:47:33 -0500

    added searchbar component

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import SearchBar from './SearchBar';
+import React from "react";
+import SearchBar from "./SearchBar";
 
 const App = () => {
-  return (
-    <div>
-      App
-    </div>
-  );
+	return (
+		<div>
+			<SearchBar />
+		</div>
+	);
 };
 
 export default App;

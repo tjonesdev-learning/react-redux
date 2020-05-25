f175dbebc334dad3899fd735f881ebd3a8001472
Tracey <tracey.l.jones@outlook.com>
2020-05-24 20:23:21 -0500

    Moved geolocation call to constructor function

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import SeasonDisplay from './SeasonDisplay';
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
 /* 
   const App = () => {
@@ -17,20 +17,20 @@ import SeasonDisplay from './SeasonDisplay';
 
 // Re-factored to class based component
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {lat: null};
-  }
+		this.state = { lat: null };
 
-  render() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
-    );
+		window.navigator.geolocation.getCurrentPosition(
+			(position) => console.log(position),
+			(err) => console.log(err)
+		);
+	}
 
-    return <div>Latitude: </div>
-  };
+	render() {
+		return <div>Latitude: {this.state.lat}</div>;
+	}
 }
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));

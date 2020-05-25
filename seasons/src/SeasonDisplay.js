dbe82ad2dddad853da791441db71b7b9d650285f
Tracey <tracey.l.jones@outlook.com>
2020-05-25 13:53:08 -0500

    convert season month to specific text depending on season

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -10,11 +10,10 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
-
-  console.log(season);
+  const seasonText = season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach';
 
   return (
-    <div>Season Display</div>
+  <div>{seasonText}</div>
   );
 };
 

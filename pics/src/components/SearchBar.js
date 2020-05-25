7d926b4dcbbed5e9b7e78515d625deddd6cb4dcc
Tracey <tracey.l.jones@outlook.com>
2020-05-25 17:10:09 -0500

    added callback function

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,13 +1,15 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+	onInputChange(event) {}
+
 	render() {
 		return (
 			<div className="ui segment">
 				<form className="ui form">
 					<div className="field">
 						<label>Image Seach</label>
-						<input type="text" />
+						<input type="text" onChange={this.onInputChange} />
 					</div>
 				</form>
 			</div>

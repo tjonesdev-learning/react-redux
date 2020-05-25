c9550d4b42d4e51097d5c4b80af95a3e56fe397a
Tracey <tracey.l.jones@outlook.com>
2020-05-25 16:08:50 -0500

    moved app component to separate file and imported to index

--- pics/src/index.js
+++ pics/src/index.js
@@ -1,10 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App';
 
-class App extends React.Component {
-  state = {}
-  
-  render() {
-    return 
-  }
-}
+ReactDOM.render(<App />, document.querySelector('#root'));

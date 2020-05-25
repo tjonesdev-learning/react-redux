aa743a9123428499c32e481fd8d8831b4454ac98
Tracey <tracey.l.jones@outlook.com>
2020-05-25 13:43:54 -0500

    sending lat info to seasondisplay component

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -41,7 +41,7 @@ class App extends React.Component {
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>
+      return <SeasonDisplay lat={this.state.lat} />
     }
 
     return <div>Loading!</div>

0f2ed01da80f259e71abc0c57ed5ec01827ba962
Tracey <tracey.l.jones@outlook.com>
2020-05-29 17:02:55 -0500

    moved api params and api key to app file to get around axios bug/error

--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -1,13 +1,5 @@
 import axios from "axios";
 
-const KEY = "AIzaSyBEx_YYnT3R1K1LNmcrDHCtP4vcbaNQn1Y";
-
 export default axios.create({
 	baseURL: "https://www.googleapis.com/youtube/v3",
-	params: {
-		part: "snippet",
-		type: "video",
-		maxResults: 5,
-		key: KEY,
-	},
 });
--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
 
+const KEY = "AIzaSyBEx_YYnT3R1K1LNmcrDHCtP4vcbaNQn1Y";
+
 class App extends React.Component {
 	state = { videos: [] };
 
@@ -9,6 +11,10 @@ class App extends React.Component {
 		const response = await youtube.get("/search", {
 			params: {
 				q: term,
+				part: "snippet",
+				maxResults: 5,
+				type: "video",
+				key: KEY,
 			},
 		});
 

12e7564a77123a2e94884cf1139e51995ccff64f
Tracey <tracey.l.jones@outlook.com>
2020-05-29 17:23:26 -0500

    import videolist

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
+import VideoList from "./VideoList";
 
 const KEY = "AIzaSyBEx_YYnT3R1K1LNmcrDHCtP4vcbaNQn1Y";
 
@@ -24,8 +25,8 @@ class App extends React.Component {
 	render() {
 		return (
 			<div className="ui container">
-				<SearchBar onFormSubmit={this.onTermSubmit} />I have{" "}
-				{this.state.videos.length} videos
+				<SearchBar onFormSubmit={this.onTermSubmit} />
+				<VideoList videos={this.state.videos} />
 			</div>
 		);
 	}

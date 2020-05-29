3761463273e25c3a920929468e7180f0a3dcc04b
Tracey <tracey.l.jones@outlook.com>
2020-05-29 16:47:41 -0500

    configured for axios and youtube api

--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from "axios";
+
 const KEY = "AIzaSyBEx_YYnT3R1K1LNmcrDHCtP4vcbaNQn1Y";
+
+export default axios.create({
+	baseURL: "https://www.googleapis.com/youtube/v3",
+	params: {
+		part: "snipper",
+		maxResults: 5,
+		key: KEY,
+	},
+});

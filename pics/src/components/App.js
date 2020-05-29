e4b437353f3ee3dd6365c93e8005fd8d9bf5d0b5
Tracey <tracey.l.jones@outlook.com>
2020-05-28 20:56:59 -0500

    refactored to class component and started setup for communicating from searchbar to app

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 
-const App = () => {
-	return (
-		<div className="ui container" style={{ marginTop: "10px" }}>
-			<SearchBar />
-		</div>
-	);
-};
+class App extends React.Component {
+	onSearchSubmit(term) {}
+
+	render() {
+		return (
+			<div className="ui container" style={{ marginTop: "10px" }}>
+				<SearchBar onSubmit={this.onSearchSubmit} />
+			</div>
+		);
+	}
+}
 
 export default App;

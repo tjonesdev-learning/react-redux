92975a3e9bc8494b06c5ac1c84fbb04d6e87826b
Tracey <tracey.l.jones@outlook.com>
2020-05-29 13:47:20 -0500

    fixed arg value to be "image"

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -3,7 +3,7 @@ import React from "react";
 import ImageCard from "./ImageCard";
 
 const ImageList = (props) => {
-	const images = props.images.map(({ image }) => {
+	const images = props.images.map((image) => {
 		return <ImageCard key={image.id} image={image} />;
 	});
 

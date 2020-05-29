6b35e8a64475f8c5405ace4b679d24461ee51190
Tracey <tracey.l.jones@outlook.com>
2020-05-29 17:28:50 -0500

    import videoitem + setup list map method

--- videos/src/components/VideoList.js
+++ videos/src/components/VideoList.js
@@ -1,6 +1,11 @@
 import React from "react";
+import VideoItem from "./VideoItem";
 
 const VideoList = ({ videos }) => {
+	const renderedList = videos.map((video) => {
+		return <VideoItem />;
+	});
+
 	return <div>{videos.length}</div>;
 };
 

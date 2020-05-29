b945ea89b3c9d5b05678b7e6c28f181de95678a5
Tracey <tracey.l.jones@outlook.com>
2020-05-29 17:41:51 -0500

    more semantic styling

--- videos/src/components/VideoItem.js
+++ videos/src/components/VideoItem.js
@@ -3,8 +3,10 @@ import React from "react";
 const VideoItem = ({ video }) => {
 	return (
 		<div className="item">
-			<img src={video.snippet.thumbnails.medium.url} />
-			{video.snippet.title}
+			<img className="ui image" src={video.snippet.thumbnails.medium.url} />
+			<div className="content">
+				<div className="header">{video.snippet.title}</div>
+			</div>
 		</div>
 	);
 };

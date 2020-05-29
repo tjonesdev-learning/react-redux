d38e8497106e4aca90b55ff001224c9e6a6dfcdd
Tracey <tracey.l.jones@outlook.com>
2020-05-29 16:56:22 -0500

    added semantic styling to div

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import youtube from "../apis/youtube";
 
 class App extends React.Component {
-	onTermSubmit = (term) => {};
+	onTermSubmit = (term) => {
+		youtube.get("/search", {
+			params: {
+				q: term,
+			},
+		});
+	};
 
 	render() {
 		return (
-			<div>
+			<div className="ui container">
 				<SearchBar onFormSubmit={this.onTermSubmit} />
 			</div>
 		);

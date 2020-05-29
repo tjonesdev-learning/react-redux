6c70d70e4a18e785baa13078cfbde24b42aff9b4
Tracey <tracey.l.jones@outlook.com>
2020-05-29 16:26:31 -0500

    added semantic styling classes and form element

--- videos/src/SearchBar.js
+++ videos/src/SearchBar.js
@@ -2,7 +2,15 @@ import React from "react";
 
 class SearchBar extends React.Component {
 	render() {
-		return <div>SearchBar</div>;
+		return (
+			<div className="search-bar ui segment">
+				<form className="ui form">
+					<div className="field">
+						<label>Video Search</label>
+					</div>
+				</form>
+			</div>
+		);
 	}
 }
 

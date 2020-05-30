acc59809df69caa81793a60c9b1b61ab2547f467
Tracey <tracey.l.jones@outlook.com>
2020-05-30 14:29:24 -0500

    added onclick

--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -8,7 +8,12 @@ class SongList extends Component {
 			return (
 				<div className="item" key={song.title}>
 					<div className="right floated content">
-						<button className="ui button primary">Select</button>
+						<button
+							className="ui button primary"
+							onClick={() => this.props.selectSong(song)}
+						>
+							Select
+						</button>
 					</div>
 
 					<div className="content">{song.title}</div>

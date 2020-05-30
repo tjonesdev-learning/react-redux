5ede70a6c0cecbba1457ed1ee82fc872d4930fb2
Tracey <tracey.l.jones@outlook.com>
2020-05-30 13:48:40 -0500

    added songreducer and selectedsongreducer

---
+++ songs/src/reducers/index.js
@@ -0,0 +1,15 @@
+const songsReducer = () => {
+	return [
+		{ title: "No Scrubs", duration: "4:05" },
+		{ title: "Macarena", duration: "2:30" },
+		{ title: "I Want It That Way", duration: "1:45" },
+		{ title: "All Star", duration: "3:15" },
+	];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+	if (action.type === "SONG_SELECTED") {
+		return action.payload;
+	}
+	return selectedSong;
+};

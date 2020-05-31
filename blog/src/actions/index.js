fbce02497b81a4b687eb7a0854c3880a2c37ea72
Tracey <tracey.l.jones@outlook.com>
2020-05-31 12:49:51 -0500

    added code for memoization with lodash but commented out due to refactoring

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from "loadash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
 export const fetchPosts = () => async (dispatch) => {
@@ -6,8 +7,17 @@ export const fetchPosts = () => async (dispatch) => {
 	dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-export const fetchUser = (id) => async (dispatch) => {
+/* MEMOIZATION WITH LODASH
+-- Used to fix numerous network requests per user. 
+Refactored to use overfetching technique instead. --
+
+export const fetchUser = (id) => (dispatch) => {
+	_fetchUser(id, dispatch);
+};
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
 	const response = await jsonPlaceholder.get(`/users/${id}`);
 
 	dispatch({ type: "FETCH_USER", payload: response.data });
-};
+}); 
+*/

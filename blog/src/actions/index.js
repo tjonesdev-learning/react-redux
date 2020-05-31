edcc5096d4ce9fa916e01901e0dbcb56776d7a03
Tracey <tracey.l.jones@outlook.com>
2020-05-31 12:58:13 -0500

    added fetchpostsandusers action creator

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,10 +1,20 @@
-import _ from "loadash";
+//import _ from "loadash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+export const fetchPostsAndUsers = () => async (dispatch) => {
+	await dispatch(fetchPosts());
+};
 
 export const fetchPosts = () => async (dispatch) => {
 	const response = await jsonPlaceholder.get("/posts");
 
 	dispatch({ type: "FETCH_POSTS", payload: response.data });
+};
+
+export const fetchUser = (id) => async (dispatch) => {
+	const response = await jsonPlaceholder.get(`/users/${id}`);
+
+	dispatch({ type: "FETCH_USER", payload: response.data });
 };
 
 /* MEMOIZATION WITH LODASH

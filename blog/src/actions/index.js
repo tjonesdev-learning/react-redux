bf1358fcc4ab0f27a1e937e833e671e46c035962
Tracey <tracey.l.jones@outlook.com>
2020-05-31 13:04:39 -0500

    fixed lodash typo, added foreach to iterate over userids

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,10 +1,12 @@
-import _ from "loadash";
+import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
 export const fetchPostsAndUsers = () => async (dispatch, getState) => {
 	await dispatch(fetchPosts());
 
 	const userIds = _.uniq(_.map(getState().posts, "userId"));
+
+	userIds.forEach((id) => dispatch(fetchUser(id)));
 };
 
 export const fetchPosts = () => async (dispatch) => {
